e374db1d1e8531a3913afd3442eefc6e4aa224c1
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-01 15:50:44 -0500

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This is a small helper script to grab the current weather from wttr.in
+# This can be added to cron to run once an hour, which should be enough
+
+# Pick your city and format (wttr currently supports 1 - 4)
+URL=https://wttr.in/~New+York?format=1
+
+# Store file. This should be the same as in webcam.sh if you're using it there
+OUT=/tmp/weather.report
+
+# Create temp file to store weather
+TMP=`mktemp -t weather.XXXXXXX` || exit 1
+
+# This syntax is for OpenBSD
+ftp -o $TMP $URL || exit 1
+# On linux:
+#  wget -O $TMP $URL || exit 1
+
+mv $TMP $OUT
+
+exit
+
