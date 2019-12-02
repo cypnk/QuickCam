b2b570e11bee3a01cf05e99e7925aa559e6321ff
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-02 10:57:51 -0500

    Mixed source refresh added

--- cam.js
+++ cam.js
@@ -2,23 +2,32 @@
 (function refresh() {
 	var 
 	rte = 180000, // 3 Minute refresh rate
-	img = document.querySelectorAll( '[data-feature*="refresh"]' ),
+	ele = document.querySelectorAll( '[data-feature*="refresh"]' ),
 	utc = new Date().getTime(),
 	uri = function( u ) {
 		return u.split( '?', 1 )[0];
 	};
 	
-	for ( var i in img ) {
-		var m = img[i];
+	for ( var i in ele ) {
+		var m = ele[i];
 		
 		if ( typeof m !== 'object' ) {
 			continue;
 		}
 		
-		m.setAttribute( 
-			'src', 
-			uri( m.getAttribute( 'src' ) ) + '?' + utc
-		);
+		if ( m.hasAttribute( 'src' ) ) {
+			m.setAttribute( 
+				'src', 
+				uri( m.getAttribute( 'src' ) ) + '?' + utc
+			);
+		}
+		
+		if ( m.hasAttribute( 'href' ) ) {
+			m.setAttribute( 
+				'href', 
+				uri( m.getAttribute( 'href' ) ) + '?' + utc
+			);
+		}
 	}
 	
 	setTimeout( refresh, rte );
