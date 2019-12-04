e7162b6ca36a14930d965366822a217d191de37f
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-04 07:44:22 -0500

    Added gateway timeout error check

--- weather.sh
+++ weather.sh
@@ -18,6 +18,10 @@ ftp -o $TMP $URL || rm $TMP
 #  wget -O $TMP $URL || rm $TMP
 
 if [ -f $TMP ]; then
+	# Check for gateway timeout error
+	if grep -q "504" $TMP; then
+		rm $TMP && exit
+	fi
 	mv $TMP $OUT
 fi
 
