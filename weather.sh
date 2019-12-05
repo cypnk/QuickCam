5cc532398f66245925b3fcae9857a421cae08c77
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-05 08:43:22 -0500

    Added format description, improved fault tolerance

--- weather.sh
+++ weather.sh
@@ -6,6 +6,10 @@
 # Pick your city and format (wttr currently supports 1 - 4)
 URL=https://wttr.in/~New+York?format=1
 
+# Optionally, you can use ?format=%C+%t to get the weather condtion and temps
+# E.G. "Sunny +39F"
+# Read more about formats on the wttr project: https://github.com/chubin/wttr.in
+
 # Store file. This should be the same as in webcam.sh if you're using it there
 OUT=/tmp/weather.report
 
@@ -19,7 +23,7 @@ ftp -o $TMP $URL || rm $TMP
 
 if [ -f $TMP ]; then
 	# Check for gateway timeout and other errors
-	if grep -q -i "504\|unknown\|error\|found" $TMP; then
+	if grep -q -i "504\|format\|location\|error\|found" $TMP; then
 		rm $TMP && exit
 	fi
 	mv $TMP $OUT
